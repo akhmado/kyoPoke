16d97e26cc88f742389ce73ab89c22017728cfa4
akhmad <ahdmohd3@gmail.com>
2023-08-24 20:44:41 +0600

    * Added build and serve scripts

--- workspaces/backend/src/index.ts
+++ workspaces/backend/src/index.ts
@@ -3,9 +3,13 @@ import { appRouter } from "./router/index";
 import express, { Express } from "express";
 import cors from "cors";
 import { createContext } from "./trpc/context";
+import path from "path";
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+app.use(express.static("public"));
 
 app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
 
