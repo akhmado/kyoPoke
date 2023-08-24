ed60690b5d24c37f7c2b3760f95b0830460af19a
akhmad <ahdmohd3@gmail.com>
2023-08-24 21:32:57 +0600

    * Small adjustment

--- workspaces/frontend/src/main.tsx
+++ workspaces/frontend/src/main.tsx
@@ -17,7 +17,7 @@ function Main() {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: process.env.VITE_REACT_TRPC_URL || "http://localhost:8080/trpc",
+          url: `${process.env.VITE_REACT_TRPC_URL}/trpc` || "http://localhost:8080/trpc",
           //@ts-expect-error error
           async headers() {
             const token = getAuthToken();
