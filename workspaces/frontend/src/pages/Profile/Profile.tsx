cc2b5832525f03c7c7616ef1262f374170bb1ef6
akhmad <ahdmohd3@gmail.com>
2023-08-24 20:16:43 +0600

    * Password input type

--- workspaces/frontend/src/pages/Profile/Profile.tsx
+++ workspaces/frontend/src/pages/Profile/Profile.tsx
@@ -102,6 +102,7 @@ function Profile() {
             </Box>
             <Box>
               <Input
+                type="password"
                 isInvalid={!!errors.password}
                 {...register("password")}
                 placeholder="Password"
